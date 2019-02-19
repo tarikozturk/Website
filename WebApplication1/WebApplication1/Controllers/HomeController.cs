5510b568bccd75c1f8a01cfecaac5722d70b0850
Tarik Ozturk <t.ozturk@belastingdienst.nl>
2019-02-19 22:48:10 +0100

    ter

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -13,7 +13,7 @@ namespace WebApplication1.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Test()
         {
             ViewBag.Message = "Your application description page.";
 
